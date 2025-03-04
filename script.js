f7043c2d53782ae0bfd83cc2acc867023960677d
kimyen0312 <kimyen123nguyenthikimyen@gmail.com>
2025-03-04 07:53:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+function login()
+{
+  let u=username.value
+  let p=pw.value
+  if(u=="kimyen" && p=="031207")
+  {
+    window.open("home.html")
+    document.getElementById(compiler).style.display="block"
+  }
 
